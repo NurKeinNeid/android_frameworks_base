cafb3d83f2f5e8aa04ece40e4f3ce911f0a06b5d
Akash <akashniki@gmail.com>
2020-03-14 12:05:20 +0100

    Revert "SystemUI: add pulsing callback to KeyguardUpdateMonitor"
    This reverts commit 815d2f6c8ff10e2eff73371cb1061345b01a3b16.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4042,7 +4042,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     callback.onPulseStarted();
                     updateNotificationPanelTouchState();
                     setPulsing(true);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(true);
                 }
 
                 @Override
@@ -4055,7 +4054,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarWindow.suppressWakeUpGesture(false);
                     }
                     setPulsing(false);
-                    KeyguardUpdateMonitor.getInstance(mContext).setPulsing(false);
                 }
 
                 private void setPulsing(boolean pulsing) {
